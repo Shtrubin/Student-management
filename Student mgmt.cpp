2edeaf24f5b53b00f6cb704b7d560d9135a4cf1d
Rubin <rubeensht17@gmail.com>
2022-09-26 13:36:26 +0545

    initial commit

---
+++ Student mgmt.cpp
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+struct Student
+{
+    int rollnumber;
+    char name[100];
+    char phone[100];
+    float percentage;
+    struct Student *next;
+    
+}* head;
+int main()
+{
+return 0;	
+}
