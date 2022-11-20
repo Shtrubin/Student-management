19502020e13b7675ea0eceb4e7e9c164159931d6
Shtrubin <38935646+Shtrubin@users.noreply.github.com>
2022-11-20 19:18:27 +0545

    display part commit by rubin shrestha
    Assisted on displaying the student records.

--- Student mgmt.cpp
+++ Student mgmt.cpp
@@ -55,6 +55,19 @@ void Delete(int rollnumber)
     printf("Student with roll number %d is not found !!!\n", rollnumber);
     
 }
+void display()
+{
+    struct Student * temp = head;
+    while(temp!=NULL){
+        
+        printf("Roll Number: %d\n", temp->rollnumber);
+        printf("Name: %s\n", temp->name);
+        printf("Phone: %s\n", temp->phone);
+        printf("Percentage: %0.4f\n\n", temp->percentage);
+        temp = temp->next;
+        
+    }
+}
 int main()
 {
     
